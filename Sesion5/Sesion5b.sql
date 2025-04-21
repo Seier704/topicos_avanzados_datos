66ccd1ec2da5f33232792b7e83cc8aab9941ae53
Seier704 <89606997+Seier704@users.noreply.github.com>
2025-04-21 15:30:30 -0600

    Update Sesion5b.sql

--- Sesion5/Sesion5b.sql
+++ Sesion5/Sesion5b.sql
@@ -2,7 +2,7 @@ DECLARE
 CURSOR pedido_cursor IS
  SELECT PedidoID, Total
  FROM Pedidos
- WHERE Total < 400
+ WHERE Total < 500
  FOR UPDATE;
 v_pedido_id NUMBER;
 v_total NUMBER;
@@ -12,10 +12,10 @@ LOOP
  FETCH pedido_cursor INTO v_pedido_id, v_total;
  EXIT WHEN pedido_cursor%NOTFOUND;
  UPDATE Pedidos
- SET Total = v_total * 1.1
+ SET Total = v_total * 1.5
  WHERE CURRENT OF pedido_cursor;
  DBMS_OUTPUT.PUT_LINE('Pedido ' || v_pedido_id || ' actualizado a: ' || (v_total *
-1.1));
+1.5));
 END LOOP;
 CLOSE pedido_cursor;
 END;
