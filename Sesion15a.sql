37b656655c98cf7c1c55f6dc810f0d33699f402d
FABIAN ANTONIO SILVA TORO <fabian.silvat@ucen.cl>
2025-05-19 18:47:44 -0400

    Lab/ Sesion 15 primer ejercicio

---
+++ Sesion15a.sql
@@ -0,0 +1,11 @@
+--Crea un índice compuesto en la tabla
+--DetallesPedidos para las columnas PedidoID y
+--ProductoID. Luego, escribe una consulta que use
+--este índice y analiza su plan de ejecución.
+
+CREATE INDEX idx_pedidos_pedidoid_productoid ON
+DetallesPedidos(PedidoID,ProductoID);
+
+SELECT * FROM Pedidos
+WHERE PedidoID = 101 AND ProductoID = 1;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
