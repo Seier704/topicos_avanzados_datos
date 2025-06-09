688e69a48ba68e2a997e338972c849f8fd58b00a
Seier <fsilvat2000@gmail.com>
2025-06-09 01:40:24 -0400

    Lab/Sesion 15 ejercicio b motivo estudio

--- Sesion15b.sql
+++ Sesion15b.sql
@@ -10,3 +10,19 @@ CREATE TABLE Ventas (
 PARTITION BY HASH(ClienteID)
 PARTITIONS 4;
 
+SELECT 
+    PARTITION_NAME, 
+    COUNT(*) AS Registros
+FROM 
+    USER_TAB_PARTITIONS p
+JOIN 
+    TABLE(
+        SELECT /*+ dynamic_sampling(4) */ * FROM Ventas
+    ) v 
+    ON MOD(v.ClienteID, 4) = TO_NUMBER(SUBSTR(p.PARTITION_NAME, -1))
+GROUP BY PARTITION_NAME;
+
+SELECT 
+    ClienteID,
+    DUMP(ROWID) AS RawRowID
+FROM Ventas;
